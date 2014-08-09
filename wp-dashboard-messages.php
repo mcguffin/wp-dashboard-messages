709650b9891e3a216cc4a336415edc32700345e2
mcguffin <joern@flyingletters.com>
2014-08-09 18:44:06 +0200

    pretty up colors a little

--- wp-dashboard-messages.php
+++ wp-dashboard-messages.php
@@ -14,21 +14,30 @@ Author URI: https://github.com/mcguffin
 
 Text Domain: dashboardmessages
 Domain Path: /lang/
+
+
+
+green: #A3B745
+blue: #0074a2
+orange: #d46f15
+yellow: #ccaf0b
+red #e14d43
+cyan: #74B6CE
+
+		√	__('Yellow') 
+			__('Purple') 
+	√		__('Red') 	
+	√		__('Green') 
+	√		__('Blue') 	
+	√		__('Cyan') 	
+
 */
 
 
 if ( ! class_exists( 'DashboardMessages' ) ) :
 class DashboardMessages {
 	private static $_messages = null;
-	private static $color_codes = array(
-		'' 		 => array( "code"=>"" , "gradient" => "#f5f5f5,#f9f9f9"),
-		"f5f5b5" => array( "code"=>"f5f5b5" , "gradient" => array("from"=>"#f5f5b5","to"=>"#f9f9b9",'dark'=>'#e5e5a5')), 
-		"f5d5f5" => array( "code"=>"f5d5f5" , "gradient" => array("from"=>"#f5d5f5","to"=>"#f9d9f9",'dark'=>'#e5a5e5')), 
-		"f5d5d5" => array( "code"=>"f5d5d5" , "gradient" => array("from"=>"#f5d5d5","to"=>"#f9d9d9",'dark'=>'#e5a5a5')), 
-		"d5f5d5" => array( "code"=>"d5f5d5" , "gradient" => array("from"=>"#d5f5d5","to"=>"#d9f9d9",'dark'=>'#a5e5a5')),
-		"d5e5f5" => array( "code"=>"d5e5f5" , "gradient" => array("from"=>"#d5e5f5","to"=>"#d9e9f9",'dark'=>'#95b5e5')),
-		"d5f5f5" => array( "code"=>"d5f5f5" , "gradient" => array("from"=>"#d5f5f5","to"=>"#d9f9f9",'dark'=>'#a5e5e5')),
-	);
+	private static $colors;
 
 
 	// --------------------------------
@@ -41,7 +50,15 @@ class DashboardMessages {
 		add_action('admin_init',array(__CLASS__,'admin_init' ) );
 		add_action('wp_dashboard_setup',array(__CLASS__,'show_messages' ) , 1 );
 		load_plugin_textdomain( 'dashboardmessages' , false, dirname( plugin_basename( __FILE__ )) . '/lang');
-		
+		self::$colors = array(
+			""			=> array( 'label' => __('Default'),	"code"=>"" , "color" => "#f5f5f5,#f9f9f9"), // yellow
+			"ccaf0b" 	=> array( 'label' => __('Yellow'),	"code"=>"ccaf0b" , "color" => "333"), // yellow
+			"f5d5f5" 	=> array( 'label' => __('Purple'),	"code"=>"f5d5f5" , "color" => "000"), // purple
+			"e14d43"	=> array( 'label' => __('Red'),		"code"=>"e14d43" , "color" => "fff"), // red
+			"a3b745"	=> array( 'label' => __('Green'),	"code"=>"a3b745" , "color" => "fff"), // green
+			"0074a2"	=> array( 'label' => __('Blue'),	"code"=>"0074a2" , "color" => "fff"), // blue
+			"74B6CE"	=> array( 'label' => __('Cyan'),	"code"=>"74B6CE" , "color" => "000"), // cyan
+		);
 		
 	}
 	static function admin_init() {
@@ -94,28 +111,19 @@ class DashboardMessages {
 		// 
 		$post_color = get_post_meta( $post->ID , '_dashboard_color' , true );
 
-		$colors = array(
-			__('Default')	=> array( "code"=>"" , "gradient" => "#f5f5f5,#f9f9f9"), // yellow
-			__('Yellow') 	=> array( "code"=>"f5f5b5" , "gradient" => "#f5f5b5,#f9f9b9"), // yellow
-			__('Purple') 	=> array( "code"=>"f5d5f5" , "gradient" => "#f5d5f5,#f9d9f9"), // purple
-			__('Red') 		=> array( "code"=>"f5d5d5" , "gradient" => "#f5d5d5,#f9d9d9"), // red
-			__('Green') 	=> array( "code"=>"d5f5d5" , "gradient" => "#d5f5d5,#d9f9d9"), // green
-			__('Blue') 		=> array( "code"=>"d5e5f5" , "gradient" => "#d5e5f5,#d9e9f9"), // blue
-			__('Cyan') 		=> array( "code"=>"d5f5f5" , "gradient" => "#d5f5f5,#d9f9f9"), // cyan
-		);
 		
 		?><div class="misc-pub-section"><?php
 			?><h2><?php _e('Background Color') ?></h2><?php
-			foreach ( $colors as $label=>$color ) {
+			foreach ( self::$colors as $code=>$color ) {
 				extract( $color );
 				if ( $code ) {
-					?><div class="colorselect" style="background-color:#<?php echo $code ?>"><?php
+					?><div class="colorselect" style="background-color:#<?php echo $code ?>;"><?php
 					?><input type="radio" name="_dashboard_color" value="<?php echo $code ?>" id="colorselect<?php echo $code ?>" <?php checked($code,$post_color,true) ?> /><?php
 				} else {
 					?><div class="colorselect"><?php
 					?><input type="radio" name="_dashboard_color" value="<?php echo $code ?>" id="colorselect" <?php checked( $code , $post_color , true ) ?> /><?php
 				}
-					?><label for="colorselect<?php echo $code ?>"><span class="colorfield"><?php echo $label ?></span></label><?php
+					?><label for="colorselect<?php echo $code ?>"><span class="colorfield" style="color:#<?php echo $label ?>"><?php echo $label ?></span></label><?php
 				?></div><?php
 			}
 			?><div style="clear:both;"></div><?php
@@ -199,9 +207,11 @@ class DashboardMessages {
 EOT;
 echo $str;';
 			if ( current_user_can_for_blog( $post->blog_id , 'edit_post' , $post->ID ) ) {
-				$fnc_body .= "\nswitch_to_blog( {$post->blog_id} );";
+				if ( is_multisite() )
+					$fnc_body .= "\nswitch_to_blog( {$post->blog_id} );";
 				$fnc_body .= "\nedit_post_link( __('Edit this') , '<p>' , '</p>' , {$post->ID} );";
-				$fnc_body .= "\nrestore_current_blog( );";
+				if ( is_multisite() )
+					$fnc_body .= "\nrestore_current_blog( );";
 			}
 			add_meta_box( $post->dashboard_uid , $post->post_title, create_function( '' , $fnc_body ) , 'dashboard' , 'normal' , 'high' );
 		}
@@ -269,7 +279,8 @@ echo $str;';
 	static function admin_head_dashboard( ) {
 		global $wpdb;
 		$selectors = array();
-		foreach ( self::$color_codes as $code => $color) {
+		foreach ( self::$colors as $code => $item) {
+			extract($item);
 			if ( ! $code )
 				continue;
 			$selectors[$code] = array();
@@ -281,35 +292,40 @@ echo $str;';
 			$uid = $post->dashboard_uid;
 			$selectors[$post->dashboard_color][] = '#'.$uid;
 		}
-		
-		echo '<style type="text/css">';
+
+		?><style type="text/css">
+		<?php
 		
 		foreach ($selectors as $code => $selector ) {
 			if ( ! (bool) $selector )
 				continue;
-			extract(self::$color_codes[$code]["gradient"]);
+			extract(self::$colors[$code]);
 			
-			?><?php echo implode(',',$selector) ?>{
+			?>/* css generated by wp-dashboard-messages plugin */
+			<?php echo implode(',',$selector) ?>{
 	background: #<?php echo $code ?>;
-	background-image: -webkit-gradient(linear,left bottom,left top,from(<?php echo $from ?>),to(<?php echo $to ?>));
-	background-image: -webkit-linear-gradient(bottom,<?php echo $from ?>,<?php echo $to ?>);
-	background-image: -moz-linear-gradient(bottom,<?php echo $from ?>,<?php echo $to ?>);
-	background-image: -o-linear-gradient(bottom,<?php echo $from ?>,<?php echo $to ?>);
-	background-image: linear-gradient(to top,<?php echo $from ?>,<?php echo $to ?>);
+	color: #<?php echo $color ?>;
 			}
 			<?php
-
-			?><?php echo implode(' h3,',$selector) ?> h3{
-	background: #<?php echo $dark ?>;
-	background-image: -webkit-gradient(linear,left bottom,left top,from(<?php echo $to ?>),to(<?php echo $dark ?>));
-	background-image: -webkit-linear-gradient(bottom,<?php echo $to ?>,<?php echo $dark ?>);
-	background-image: -moz-linear-gradient(bottom,<?php echo $to ?>,<?php echo $dark ?>);
-	background-image: -o-linear-gradient(bottom,<?php echo $to ?>,<?php echo $dark ?>);
-	background-image: linear-gradient(to top,<?php echo $to ?>,<?php echo $dark ?>);
+			?>
+			<?php echo implode(' h3,',$selector) ?> h3{
+	background: #<?php echo $code ?>;
+	color: #<?php echo $color ?>;
+			}
+			<?php
+			?>
+			<?php echo implode(' .hndle,',$selector) ?> .hndle{
+	border-bottom-color: #<?php echo $color ?>;
+			}
+			<?php
+			?>
+			<?php echo implode(' .handlediv,',$selector) ?> .handlediv{
+	background: #<?php echo $code ?>;
+	color: #<?php echo $color ?>;
 			}
 			<?php
 		}
-		echo '</style>';
+		?></style><?php
 	}
 	
 	
