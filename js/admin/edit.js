104e82011cd53baca6b52f66ac2e9b30d213365f
mcguffin <joern@flyingletters.com>
2020-05-12 13:06:08 +0200

    dashicons search

--- js/admin/edit.js
+++ js/admin/edit.js
@@ -1,13 +1,35 @@
 (function($){
-
+	var closeDashOnEsc = function(e) {
+		if ( e.which === 27 ) {
+			closeDash();
+		}
+	}
+	var closeDash = function() {
+		$('.dashboard-messages-icons.select-window').removeClass('active');
+		$('.dashboard-messages-icons [type="search"]').val('');
+		$(document).off('keyup',closeDashOnEsc)
+	}
+	var openDash = function() {
+		$('.dashboard-messages-icons.select-window').addClass('active');
+		$(document).on('keyup',closeDashOnEsc);
+	}
 	$(document)
 		.on('click','[type="radio"]',function(e){
-			$(this).closest('.select-window').removeClass('active');
+			// hide
+			closeDash();			
 		})
 		.on('click','.select-window:not(.active)',function(e) {
-			$(this).addClass('active');
-
+			openDash();
 			e.preventDefault();
+		})
+		.on('keyup change','.dashboard-messages-icons [type="search"]', function(e) {
+			var s = $(this).val().toLowerCase();
+			$('.dashboard-messages-icons .select label').each( function(i,el) {
+				$(this).toggleClass( 
+					'-off', 
+					s !== '' && $(this).text().toLowerCase().indexOf(s) === -1 
+				);
+			})
 		})
 		.on('click', '.dashboard-message-dismiss', function(e) {
 			e.preventDefault();
