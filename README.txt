cee0f499d5e6694fc9d99798678e8138f36b6ba5
mcguffin <joern@flyingletters.com>
2022-09-22 15:46:54 +0200

    Release 1.1.6

--- README.txt
+++ README.txt
@@ -5,7 +5,7 @@ Tags: dashboard, admin
 Requires at least: 4.8
 Requires PHP: 5.6
 Tested up to: 6.0
-Stable tag: 1.1.5
+Stable tag: 1.1.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: WP Dashboard Messages
 Plugin URI: https://wordpress.org/plugins/wp-dashboard-messages
 Description: Show Messages on the WP Admin Dashboard.
 Author: Jörn Lund
-Version: 1.1.5
+Version: 1.1.6
 Author URI: https://github.com/mcguffin
 License: GPL3
 Requires WP: 4.8
